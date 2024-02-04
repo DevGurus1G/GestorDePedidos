c5bcbb9b0f4f26ef20bc6720d392928e3e595323
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2024-02-04 19:13:46 +0100

    Pedidos separado en componentes

--- Cliente/src/router/index.js
+++ Cliente/src/router/index.js
@@ -1,60 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '@/components/Login.vue';
-import ProductManagement from '@/components/ProductManagement.vue';
-import ListadoPedidos from '@/components/ListadoPedidos.vue';
-import PerfilUsuario from '@/components/PerfilUsuario.vue';
-import Inicio from '@/components/Inicio.vue';
-import RecuperarUsuario from '@/components/RecuperarUsuario.vue';
-import Contacto from '@/components/Contacto.vue';
-
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/components/Login.vue'
+import ProductManagement from '@/components/ProductManagement.vue'
+import ListadoPedidos from '@/components/ListadoPedidos.vue'
+import PerfilUsuario from '@/components/PerfilUsuario.vue'
+import Inicio from '@/components/Inicio.vue'
+import RecuperarUsuario from '@/components/RecuperarUsuario.vue'
+import Contacto from '@/components/Contacto.vue'
 
 const routes = [
   {
     path: '/',
     name: 'inicio',
     component: Inicio,
-    meta: { showFooter: true, showNav: true }, 
+    meta: { showFooter: true, showNav: true }
   },
   {
     path: '/login',
     name: 'login',
     component: Login,
-    meta: { showFooter: false, showNav: false }, // Para no ver el footer y el nav en el login
+    meta: { showFooter: false, showNav: false } // Para no ver el footer y el nav en el login
   },
   {
     path: '/product-management',
     name: 'product-management',
     component: ProductManagement,
-    meta: { requiresAuth: true, showFooter: true, showNav: true }, // Asegura que solo usuarios autenticados pueden acceder y que se vea el footer y el nav
+    meta: { requiresAuth: true, showFooter: true, showNav: true } // Asegura que solo usuarios autenticados pueden acceder y que se vea el footer y el nav
   },
   {
     path: '/listado-pedidos',
     name: 'listado-pedidos',
-    component: ListadoPedidos,
-    meta: { requiresAuth: true, showFooter: true, showNav: true }, 
+    component: () => import('../views/PedidosView.vue'),
+    meta: { requiresAuth: true, showFooter: true, showNav: true }
   },
   {
     path: '/perfil-usuario',
     name: 'perfil-usuario',
     component: PerfilUsuario,
-    meta: { requiresAuth: true, showFooter: true, showNav: true },
+    meta: { requiresAuth: true, showFooter: true, showNav: true }
   },
   {
     path: '/recuperar',
     name: 'recuperar',
-    component: RecuperarUsuario,
+    component: RecuperarUsuario
   },
   {
     path: '/contacto',
     name: 'contacto',
     component: Contacto,
-    meta: { showFooter: true, showNav: true },
-  },
-];
+    meta: { showFooter: true, showNav: true }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
